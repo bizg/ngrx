e27d44ea053a6c593b6071d0422e44d9c46cdc31
Mike <mikecardonar@gmail.com>
2023-05-04 16:52:00 -0500

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './feature/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  {
+    path: 'login',
+    loadChildren: () => import('./feature/login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./feature/dashboard/dashboard.module').then(m => m.DashboardModule)
+  }
+  // { path: 'dashboard', component: DashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
+import { metaReducers, reducers } from './app-state';
+import { TaskEffect } from './app-state/effects/task.effect';
+import { UserEffect } from './app-state/effects/user.effect';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { LoginModule } from './feature/login/login.module';
+import { DashboardModule } from './feature/dashboard/dashboard.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    SharedModule,
+    FormsModule,
+    DashboardModule,
+    LoginModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers
+    }),
+    EffectsModule.forRoot([UserEffect, TaskEffect])
   ],
   providers: [],
   bootstrap: [AppComponent]
